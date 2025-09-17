6acd5366567cd637a12298ece3e5e9147f6502ba
mikesteroonie <michaelkimhyun1113@gmail.com>
2025-09-16 23:32:32 -0700

    reo stuff

---
+++ fern/custom.js
@@ -0,0 +1,13 @@
+// Reo.dev tracking script
+!(function () {
+  var e, t, n;
+  (e = "b38ba24420b76f6"),
+    (t = function () {
+      Reo.init({ clientID: "b38ba24420b76f6" });
+    }),
+    ((n = document.createElement("script")).src =
+      "https://static.reo.dev/" + e + "/reo.js"),
+    (n.defer = !0),
+    (n.onload = t),
+    document.head.appendChild(n);
+})();
