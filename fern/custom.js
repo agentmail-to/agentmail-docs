cc8413be45486327b8ad7441e8c19f5454cb445d
mikesteroonie <michaelkimhyun1113@gmail.com>
2026-08-05 13:05:46 -0700

    fix docs rendering by removing Reo script

--- fern/custom.js
+++ fern/custom.js
@@ -6,17 +6,3 @@
   link.href = "/llms.txt";
   document.head.appendChild(link);
 })();
-
-// Reo.dev tracking script
-!(function () {
-  var e, t, n;
-  (e = "b38ba24420b76f6"),
-    (t = function () {
-      Reo.init({ clientID: "b38ba24420b76f6" });
-    }),
-    ((n = document.createElement("script")).src =
-      "https://static.reo.dev/" + e + "/reo.js"),
-    (n.defer = !0),
-    (n.onload = t),
-    document.head.appendChild(n);
-})();
